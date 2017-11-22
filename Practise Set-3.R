943ab85e943551856accad61358a3e3302d9606e
Swapnil Shashank Parkhe <sparkh3@uic.edu>
2017-11-22 03:30:19 -0600

    Third Commit

--- Practise Set-3.R
+++ Practise Set-3.R
@@ -1,5 +1,5 @@
 ###############################################################################
-#PRACTISE SET-2 (books: Elements of Stats. Learning, & Intro to Stats. Learning)
+#PRACTISE SET-3 (books: Elements of Stats. Learning, & Intro to Stats. Learning)
 ###############################################################################
 
 
